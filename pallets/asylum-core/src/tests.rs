5f1e2d4d82f9a49ece5655cb359ec6e3d3e32065
SupremaLex <georglutsenko@gmail.com>
2022-02-10 12:15:24 +0200

    First tests

--- pallets/asylum-core/src/tests.rs
+++ pallets/asylum-core/src/tests.rs
@@ -1,8 +1,6 @@
-use crate::mock::*;
-use asylum_traits::{GameInfo, ItemInfo};
-use frame_support::{assert_ok, BoundedVec};
-
-type KeyLimitOf = BoundedVec<u8, KeyLimit>;
+use crate::{mock::*, Error};
+use asylum_traits::IntepretationTypeInfo;
+use frame_support::{assert_noop, assert_ok, traits::Get, BoundedVec};
 
 macro_rules! bvec {
 	($( $x:tt )*) => {
@@ -10,106 +8,54 @@ macro_rules! bvec {
 	}
 }
 
-fn initialize_collections() {
-	assert_ok!(AsylumCore::initialize_games_collection(Origin::signed(ALICE), ALICE, ALICE));
-	assert_ok!(AsylumCore::initialize_items_collection(Origin::signed(ALICE), ALICE, ALICE));
+fn bounded<T>(string: &str) -> BoundedVec<u8, T>
+where
+	T: Get<u32>,
+{
+	TryInto::<BoundedVec<u8, T>>::try_into(string.as_bytes().to_vec()).unwrap()
 }
 
+const INTERPRETATION_TYPE_NAME_1: &str = "2D";
+const TEMPLATE_NAME_1: &str = "Sword";
+const TEMPLATE_NAME_2: &str = "Staff";
+const TEMPLATE_NAME_3: &str = "Rags";
+const MOCK_METADATA: &str = "ipfs://hash";
+
 #[test]
-fn should_mint_item() {
+fn should_create_interpretation_type() {
 	ExtBuilder::default().build().execute_with(|| {
-		initialize_collections();
-		assert_ok!(AsylumCore::mint_item(Origin::signed(ALICE), BOB, Some(bvec![0u8; 9])));
-		assert_eq!(AsylumCore::items(0), Some(ItemInfo { metadata: Some(bvec![0u8; 9]) }));
+		let name = bounded(INTERPRETATION_TYPE_NAME_1);
+		let metadata = bounded(MOCK_METADATA);
+		assert_ok!(AsylumCore::create_interpretation_type(
+			Origin::signed(ALICE),
+			name.clone(),
+			metadata.clone()
+		));
+		let id = AsylumCore::interpretation_type_id(name).unwrap();
+		assert_eq!(
+			AsylumCore::interpretation_type_info(id).unwrap(),
+			IntepretationTypeInfo { metadata }
+		);
 	});
 }
 
 #[test]
-fn should_burn_item() {
+fn should_fail_interpretation_type_1() {
 	ExtBuilder::default().build().execute_with(|| {
-		initialize_collections();
-		assert_ok!(AsylumCore::mint_item(Origin::signed(ALICE), BOB, Some(bvec![0u8; 9])));
-		assert_ok!(AsylumCore::burn_item(Origin::signed(ALICE), 0));
-	});
-}
-
-#[test]
-fn should_transfer_item() {
-	ExtBuilder::default().build().execute_with(|| {
-		initialize_collections();
-		assert_ok!(AsylumCore::mint_item(Origin::signed(ALICE), BOB, Some(bvec![0u8; 9])));
-		assert_ok!(AsylumCore::transfer_item(Origin::signed(BOB), 0, CHARLIE));
-		assert_eq!(Uniques::owner(1, 0), Some(CHARLIE));
-	});
-}
-
-#[test]
-fn should_set_item_metadata() {
-	ExtBuilder::default().build().execute_with(|| {
-		initialize_collections();
-		assert_ok!(AsylumCore::mint_item(Origin::signed(ALICE), BOB, None));
-		assert_ok!(AsylumCore::set_item_metadata(Origin::signed(ALICE), 0, bvec![0u8; 9]));
-		assert_eq!(AsylumCore::items(0), Some(ItemInfo { metadata: Some(bvec![0u8; 9]) }));
-	});
-}
-
-#[test]
-fn should_clear_item_metadata() {
-	ExtBuilder::default().build().execute_with(|| {
-		initialize_collections();
-		assert_ok!(AsylumCore::mint_item(Origin::signed(ALICE), BOB, Some(bvec![0u8; 9])));
-		assert_ok!(AsylumCore::clear_item_metadata(Origin::signed(BOB), 0));
-		assert_eq!(AsylumCore::items(0), None);
-	});
-}
-
-#[test]
-fn should_set_item_attribute() {
-	ExtBuilder::default().build().execute_with(|| {
-		initialize_collections();
-		assert_ok!(AsylumCore::mint_item(Origin::signed(ALICE), BOB, Some(bvec![0u8; 9])));
-		assert_ok!(AsylumCore::set_item_attribute(Origin::signed(ALICE), 0, bvec![1u8; 32], bvec![0u8; 9]));
-		assert_eq!(AsylumCore::attributes::<u32, KeyLimitOf>(0, bvec![1u8; 32]), Some(bvec![0u8; 9]));
-		assert_eq!(AsylumCore::attributes::<u32, KeyLimitOf>(0, bvec![2u8; 32]), None);
-	});
-}
-
-#[test]
-fn should_clear_item_attribute() {
-	ExtBuilder::default().build().execute_with(|| {
-		initialize_collections();
-		assert_ok!(AsylumCore::mint_item(Origin::signed(ALICE), BOB, Some(bvec![0u8; 9])));
-		assert_ok!(AsylumCore::set_item_attribute(Origin::signed(ALICE), 0, bvec![1u8; 32], bvec![0u8; 9]));
-		assert_eq!(AsylumCore::attributes::<u32, KeyLimitOf>(0, bvec![1u8; 32]), Some(bvec![0u8; 9]));
-		assert_ok!(AsylumCore::clear_item_attribute(Origin::signed(ALICE), 0, bvec![1u8; 32]));
-		assert_eq!(AsylumCore::attributes::<u32, KeyLimitOf>(0, bvec![1u8; 32]), None);
-	});
-}
-
-#[test]
-fn should_mint_game() {
-	ExtBuilder::default().build().execute_with(|| {
-		initialize_collections();
-		assert_ok!(AsylumCore::mint_game(Origin::signed(ALICE), BOB, bvec![0u8; 9]));
-		assert_eq!(AsylumCore::games(0), Some(GameInfo { metadata: bvec![0u8; 9] }));
-	});
-}
-
-#[test]
-fn should_burn_game() {
-	ExtBuilder::default().build().execute_with(|| {
-		initialize_collections();
-		assert_ok!(AsylumCore::mint_game(Origin::signed(ALICE), BOB, bvec![0u8; 9]));
-		assert_ok!(AsylumCore::burn_game(Origin::signed(ALICE), 0));
-	});
-}
-
-#[test]
-fn should_transfer_game() {
-	ExtBuilder::default().build().execute_with(|| {
-		initialize_collections();
-		assert_ok!(AsylumCore::mint_game(Origin::signed(ALICE), BOB, bvec![0u8; 9]));
-		assert_ok!(AsylumCore::transfer_game(Origin::signed(BOB), 0, CHARLIE));
-		assert_eq!(Uniques::owner(2, 0), Some(CHARLIE));
+		let name = bounded(INTERPRETATION_TYPE_NAME_1);
+		let metadata = bounded(MOCK_METADATA);
+		assert_ok!(AsylumCore::create_interpretation_type(
+			Origin::signed(ALICE),
+			name.clone(),
+			metadata.clone()
+		));
+		assert_noop!(
+			AsylumCore::create_interpretation_type(
+				Origin::signed(ALICE),
+				name.clone(),
+				metadata.clone()
+			),
+			Error::<Test>::InterpretationTypeAlreadyExist
+		);
 	});
 }
